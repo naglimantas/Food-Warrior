a8f001b3d8782e5add50575891b3e030149f3284
Lukas <39262485+lukaskornis@users.noreply.github.com>
2024-09-22 12:24:27 +0300

    Async support, Spawner update

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -2,15 +2,21 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    public GameObject prefab;
+    public GameObject[] prefabs;
 
     void Start()
     {
-        InvokeRepeating("Spawn",1f,1f);
+        Spawn();
     }
 
-    void Spawn()
+    async void Spawn()
     {
-        Instantiate(prefab,transform.position,Quaternion.identity);
+        while (true)
+        {
+            await new WaitForSeconds(Random.Range(0.5f,2f));
+            GameObject obj = prefabs[Random.Range(0, prefabs.Length)];
+            Vector3 pos = transform.position + Vector3.right * Random.Range(-5f, 5f);
+            Instantiate(obj, pos, Quaternion.identity);
+        }
     }
 }
